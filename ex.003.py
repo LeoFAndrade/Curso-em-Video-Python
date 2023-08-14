1e528ca515bb983fe53257ab9e9ee9b232222324
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-14 17:19:59 -0300

    Programa simples de reconhecimento de caracteres. Versão aprimorada com mais informações acerca do caractere.

--- ex.003.py
+++ ex.003.py
@@ -1,2 +1,5 @@
 n = input('Digite qualquer coisa')
 print('Tipo primitivo;', type(n)), print('É numérico ?', n.isnumeric()), print('É alfabético ?', n.isalpha())
+print('É alfanumérico ?', n.isalnum()), print('É decimal ?', n.isdecimal()), print('Está em maiúsculo ?', n.isupper())
+print('Está em minusculo ?', n.islower()), print('Está capitalizada ?', n.istitle())
+print('Só tem espaços ?', n.isspace())
