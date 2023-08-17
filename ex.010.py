a3967b8b46d8b36f8daf772835066ebe6bca123d
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-17 14:28:20 -0300

    Programa que exibe a quantidade de litros necessários para se pintar uma parede.

---
+++ ex.010.py
@@ -0,0 +1,4 @@
+l = float(input('Digite a largura da parede'))
+m = float(input('Agora digite a altura'))
+print(f'A dimensão da parede é de {l:.1f}x{m:.1f} e a sua área é de {l*m:.1f}')
+print(f'Portanto você precisará de {(l*m)/2:.2f}l de tinta para pintar a parede')
