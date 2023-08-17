afd3b681cdfb70db8318479fb9d158af8b606564
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-17 14:09:38 -0300

    Programa que exibe o valor da conversão de R$ para US$.

--- ex.008.py
+++ ex.008.py
@@ -1,13 +1,4 @@
 n = int(input('Digite um número para saber a sua tabuada:'))
-print(
-    f'''
-    {n} X {1} = {n*1}
-    {n} X {2} = {n*1}
-    {n} X {3} = {n*1}
-    {n} X {4} = {n*1}
-    {n} X {5} = {n*1}
-    {n} X {6} = {n*1}
-    {n} X {7} = {n*1}
-    {n} X {8} = {n*1}
-    {n} X {9} = {n*1}
-    ''')
+print(f'{n} x 1 = {n*1}'), print(f'{n} x 2 = {n*2}'), print(f'{n} x 3 = {n*3}'), print(f'{n} x 4 = {n*4}')
+print(f'{n} x 5 = {n*5}'), print(f'{n} x 6 = {n*6}'), print(f'{n} x 7 = {n*7}'), print(f'{n} x 8 = {n*8}')
+print(f'{n} x 9 = {n*9}'), print(f'{n} x 10 = {n*10}')
