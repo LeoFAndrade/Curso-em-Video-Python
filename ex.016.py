d2a67715aabff80dc09f2c8d4236b8cd51c71069
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-20 13:30:43 -0300

    Calculadora de hipotenusa simples.

---
+++ ex.016.py
@@ -0,0 +1,6 @@
+import math
+
+catetop = float(input(f'Comprimento do cateto oposto:'))
+catetoad = float(input(f'Comprimento do cateto adjacente:'))
+hipotenusa = math.hypot(catetop, catetoad)
+print(f'A hipotenusa mede: {hipotenusa:.2f}')
