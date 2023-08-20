55129fd559703508fcbc2c321964efc0ddfca944
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-20 20:15:34 -0300

    Programa que escolhe randomicamente algo de uma lista.

---
+++ ex.018.py
@@ -0,0 +1,8 @@
+import random
+
+nome1 = input('Primeiro aluno:')
+nome2 = input('Segundo aluno:')
+nome3 = input('Terceiro aluno:')
+nome4 = input('Quarto nome:')
+lista = [nome1, nome2, nome3, nome4]
+print(f'O aluno escolhido foi:{random.choice(lista)}')
