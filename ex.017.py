a7d7259d6d84684839adc1e9f8156283e9150854
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-20 13:56:08 -0300

    Calculadora de seno, cosseno e tangente simples.

---
+++ ex.017.py
@@ -0,0 +1,8 @@
+import math
+angulo = float(input('Digite o ângulo:'))
+SENO = math.sin(math.radians(angulo))
+print(f'O Ângulo de {angulo} tem o SENO de {SENO:.2f}')
+COSSENO = math.cos(math.radians(angulo))
+print(f'O COSSENO de {angulo} tem é de {COSSENO:.2f}')
+TANGENTE = math.tan(math.radians(angulo))
+print(f'E a TANGENTE de {angulo} é {TANGENTE:.2f}')
