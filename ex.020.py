6db5af9f63362188d5aeded71f7c76fc331d4d88
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-23 15:54:30 -0300

    Programa que exibe informações simples sobre o nome digitado.

--- ex.020.py
+++ ex.020.py
@@ -1,2 +1,6 @@
-texto = input('Digite qualquer coisa')
-print(f'O texto escrito tem:{len(texto)} strings')
+nome = (input('Digite seu nome completo:'))
+print(f'O seu nome em letra minúscula fica como:{nome.upper()}')
+print(f'Seu nome em minúsculo fica assim:{nome.lower()}')
+print(f'O seu nome tem ao todo: {len(nome) - nome.count(" ")} letras')
+separacao = nome.split()
+print(f'O seu primeiro nome tem: {len(separacao[0])} letras')
