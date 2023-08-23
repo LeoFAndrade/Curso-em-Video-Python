5457a557f2eda92c857e5caf3345fc136ab8b255
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-23 15:57:36 -0300

    Calculadora simples de desconto.

--- ex.011.py
+++ ex.011.py
@@ -1,3 +1,3 @@
 produto = float(input('Digite o preço do produto:'))
 desconto = float(input('Digite a porcentagem do desconto:'))
-print(f'O preço do produto com desconto é: {produto-(produto*desconto/100):.2f}R$')
+print(f'O preço do produto com desconto é:{produto-(produto*desconto/100):.2f}R$')
