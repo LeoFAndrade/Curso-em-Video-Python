73cbee1ad7b716cfc168c6aa8e2ae841b0221972
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-08-24 17:24:23 -0300

    Programa simples que separa dígitos de números. (não completo)

---
+++ ex.021.py
@@ -0,0 +1,3 @@
+numero = input('Digite um número qualquer:')
+divisao = [int(x) for x in str(numero)]
+print(f"""A unidade é:{divisao}, A dezena é:{divisao}, a centena é:{divisao}, E a milhar é:{divisao}""")
