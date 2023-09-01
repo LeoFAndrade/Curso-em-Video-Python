620524403db0a250ebcfe3f1f2698d0a6bb87e10
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-01 15:28:32 -0300

    Programa simples de multas eletrônica.

---
+++ ex.027.py
@@ -0,0 +1,8 @@
+velocidade = float(input('Qual a velocidade atual do carro:'))
+if velocidade > 80.00:
+    multa = (velocidade - 80.00) * 7
+    print('Você ultrapassou o limite de velocidade permitido de 80Km/h!')
+    print(f'Você deve pagar uma multa de {multa}R$')
+else:
+    print('Tenha um bom dia, dirija com segurança')
+print('Dirija com cuidado tenha um bom dia')
