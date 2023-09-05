3e5564d6d8d7fbb0558b781f5730c0fab63dc021
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-05 15:04:47 -0300

    Programa que exibe qual número é o maior eo menor.

---
+++ ex.31.py
@@ -0,0 +1,17 @@
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite o segundo:'))
+n3 = int(input('Digite o terceiro:'))
+# Verificando o menor!
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+# Verificando agora o maior!
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print(f'O menor número é {menor}')
+print(f'O maior número é {maior}')
