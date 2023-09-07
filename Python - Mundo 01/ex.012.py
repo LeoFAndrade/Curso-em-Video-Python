cec3d3d39f391e051a451c58a2da621c8dd18261
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-07 18:44:22 -0300

    Programa que exibe o salário após o aumento.

--- Python - Mundo 01/ex.012.py
+++ Python - Mundo 01/ex.012.py
@@ -1,3 +1,4 @@
 salario = float(input('Digite o salário do funcionário:'))
 aumento = float(input('Digite a porcentagem do aumento:'))
+# Verificando o salário após o aumento.
 print(f'O aumento do salário é: {salario + (salario * aumento / 100):.2f}R$')
