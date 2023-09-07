fc10b92a1470bb68adac5d576c43cc49803a9047
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-07 18:39:41 -0300

    Programa que exibe o valor da conversão de R$ para US$.

--- Python - Mundo 01/ex.009.py
+++ Python - Mundo 01/ex.009.py
@@ -1,3 +1,3 @@
 m = float(input('Digite o valor disponível em reserva R$:'))
 print(f'R${m} pode comprar em dólar: US${m / 4.96:.2f}')
-# Obrigado Plano real e Presidentes da Sexta República Brasileira.
+# Obrigado Plano real e Presidentes da Sexta República Brasileira
