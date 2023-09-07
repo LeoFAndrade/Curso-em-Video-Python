b0981adef2ad1db01bfd095ca31a4613330b82f5
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-07 16:50:52 -0300

    .

--- Python - Mundo 01/ex.014.py
+++ Python - Mundo 01/ex.014.py
@@ -1,4 +1,3 @@
 dia = int(input('Por quantos dias o carro foi alugado ?'))
 km = float(input('Quantos Km foram rodados ?'))
 pago = (dia * 60) + (km * 0.15)
-print(f'O total à ser pago é de {pago:.2f}')
