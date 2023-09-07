140e5f18de050d9ff7aed54b35be0211cb497e3a
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-07 18:43:31 -0300

    Programa que exibe o preço de um produto após o desconto.

--- Python - Mundo 01/ex.011.py
+++ Python - Mundo 01/ex.011.py
@@ -1,3 +1,4 @@
 produto = float(input('Digite o preço do produto:'))
 desconto = float(input('Digite a porcentagem do desconto:'))
+# Verificando o preço final do produto com desconto.
 print(f'O preço do produto com desconto é:{produto-(produto*desconto/100):.2f}R$')
