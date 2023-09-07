64589743bc7580ca16ad66182df71f740a004965
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-07 19:05:46 -0300

    Programa que trunca o valor decimal digitado.

--- Python - Mundo 01/ex.015.py
+++ Python - Mundo 01/ex.015.py
@@ -1,4 +1,5 @@
 import math
+# Truncando o valor decimal.
 numero = float(input('Digite um número decimal qualquer:'))
 truncado = math.trunc(numero)
 print(f'A porção inteira do número digitado {numero} é {truncado}')
