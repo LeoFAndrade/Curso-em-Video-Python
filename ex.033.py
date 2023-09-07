416dd89a7ef5d2408e18c627acb8405e5b12112f
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-07 16:23:12 -0300

    Programa que exibe se é possível formar um triângulo com medidas especificadas.

---
+++ ex.033.py
@@ -0,0 +1,14 @@
+r1 = int(input('Digite o tamanho da primeira reta:'))
+r2 = int(input('Digite o tamanho da segunda:'))
+r3 = int(input('Digite o tamanho da última reta:'))
+# Analisaremos se as três retas podem formar um triângulo ou não.
+maior = r1
+if r2 and r3 > r1:
+    maior = r2
+if r3 and r2 > r1:
+    maior = r3
+if r1 and r3 > r2:
+    print('É possível formar um triângulo com estas retas')
+# Aqui o programa vai analisar se duas retas
+else:
+    print('Não é possível formar um triângulo com estas retas')
