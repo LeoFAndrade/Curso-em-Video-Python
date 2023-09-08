7826b061e90a2b13438e10812bc1ac5f33e7973b
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:49:04 -0300

    Programa que calcula aumento de salário. (v2)

--- Python - Mundo 01/ex.032.py
+++ Python - Mundo 01/ex.032.py
@@ -2,8 +2,8 @@ salario = float(input('Digite o valor do salário:'))
 # Verificando se o tamanho do salário é superior à 1.250R$ e calculando seu aumento de 10%
 if salario > 1.250:
     salario = salario + (salario*10)/100
-    print(f'O aumento ficou no total: {salario:.0f}R$')
+    print(f'O aumento ficou no total: R${salario:.0f}')
 # Verificando se o tamanho do salário é igual ou menor à 1.250, acrescentando o aumento de 15%
 if salario <= 1.250:
     salario = salario + (salario*15)/100
-    print(f'O aumento ficou no total: {salario:.0f}R$')
+    print(f'O aumento ficou no total: R${salario:.0f}')
