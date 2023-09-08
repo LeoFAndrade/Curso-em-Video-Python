0bc47f8ac942d712e7fa4d4ffbb7dea93ec0269f
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:10:57 -0300

    Programa que separa os dígitos de um número.

--- Python - Mundo 01/ex.021.py
+++ Python - Mundo 01/ex.021.py
@@ -1,9 +1,10 @@
 numero = int(input('Digite um número qualquer:'))
-u = numero//1 % 10
-d = numero//10 % 10
-c = numero//100 % 10
-m = numero//1000 % 10
-print(f'A unidade do número é: {u}')
-print(f'A dezena do número é: {d}')
-print(f'A centena do número é: {c}')
-print(f'A milhar do número é: {m}')
+# O programa vai calcular respectivamente cada unidade de medida.
+unidade = numero//1 % 10
+dezena = numero//10 % 10
+centena = numero//100 % 10
+milhar = numero//1000 % 10
+print(f'A unidade do número é: {unidade}')
+print(f'A dezena do número é: {dezena}')
+print(f'A centena do número é: {centena}')
+print(f'A milhar do número é: {milhar}')
