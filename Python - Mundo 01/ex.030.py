fa41c3303e5ebc625daff61901f1295fdcd9fd3c
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:46:59 -0300

    Programa que exibe se um ano é bissexto ou não.

--- Python - Mundo 01/ex.030.py
+++ Python - Mundo 01/ex.030.py
@@ -1,5 +1,5 @@
 ano = int(input('Digite o ano:'))
 if ano % 4 == 0:
-    print(f'O ano "{ano}" é bissexto!')
+    print(f'O ano "{ano}" é bissexto!!!')
 else:
-    print(f'O ano "{ano}" não é bissexto!')
+    print(f'O ano "{ano}" não é bissexto!!!')
