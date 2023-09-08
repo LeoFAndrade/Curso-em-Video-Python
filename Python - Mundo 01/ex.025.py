ca12c2ce23ecb482e3f4c24b9a427ae87ba9d76d
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:29:26 -0300

    Programa que exibe o nome e o último nome.

--- Python - Mundo 01/ex.025.py
+++ Python - Mundo 01/ex.025.py
@@ -1,4 +1,5 @@
 nome = input('Digite o seu nome:').title().split()
+# Programa que exibe o nome e o último nome.
 print(f'Bom te conhecer {nome[0]}!')
 print(f'Seu primeiro nome é "{nome[0]}"')
 print(f'E seu último nome é "{nome[-1]}"')
