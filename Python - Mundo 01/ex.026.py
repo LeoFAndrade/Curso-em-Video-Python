08d2598947be6919f29628358385fb3ff3092d36
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:33:06 -0300

    Jogo simples de adivinhação.

--- Python - Mundo 01/ex.026.py
+++ Python - Mundo 01/ex.026.py
@@ -1,11 +1,14 @@
 import random
 from time import sleep
 print('Vamos brincar de adivinhar números!')
+# O jogador começa escolhendo algum número de 0 a 5.
 numero = int(input('Digite um número de 0 à 5:'))
 listanum = [0, 1, 2, 3, 4, 5]
 PC = random.choice(listanum)
+# Aqui o programa vai escolher aleatoriamente qualquer dos números listados acima.
 print('PROCESSANDO...')
 sleep(3)
+# O programa finaliza mostrando o resultado, e mostra uma mensagem diferente dependendo do resultado.
 print(f'{PC}')
 if numero == PC:
     print('Droga, você acertou!')
