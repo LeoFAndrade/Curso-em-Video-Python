1a875d35efacdfe6cc763895245f5bb29ecfac98
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:38:16 -0300

    Programa simples de multas eletrônica.

--- Python - Mundo 01/ex.027.py
+++ Python - Mundo 01/ex.027.py
@@ -1,4 +1,5 @@
 velocidade = float(input('Qual a velocidade atual do carro:'))
+# O programa verifica se a velocidade é superior à 80Km, se for a multa é de 7.00R$ por cada km acima do limite.
 if velocidade > 80.00:
     multa = (velocidade - 80.00) * 7
     print('Você ultrapassou o limite de velocidade permitido de 80Km/h!')
