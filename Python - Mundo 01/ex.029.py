b9885bb249e3537472545e13c2cf2f0c49392213
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-08 11:46:21 -0300

    Programa de cálculo de preço de viagens.

--- Python - Mundo 01/ex.029.py
+++ Python - Mundo 01/ex.029.py
@@ -1,5 +1,6 @@
 km = float(input('Qual é a distância da viagem ?'))
+# Calcula o preço de uma viagem, se for superior à 200Km, a cobrança será de 0.45R$ ao invés de 0.50R$.
 if km <= 200.00:
-    print(f'A Sua viagem ficou: {km * 0.50}R$')
+    print(f'A Sua viagem ficou: R${km * 0.50}')
 else:
-    print(f'A Sua viagem ficou: {km * 0.45}R$')
+    print(f'A Sua viagem ficou: R${km * 0.45}')
