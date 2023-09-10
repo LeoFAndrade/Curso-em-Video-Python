cbc8c7699d7ae78c4275c113c668e089e2c888d4
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-10 18:58:06 -0300

    Adicionado cores.

--- Python - Mundo 01/ex.001.py
+++ Python - Mundo 01/ex.001.py
@@ -1,2 +1,2 @@
-nome = input('Qual é o seu nome ?')
-print(f'Olá {nome}, seja bem muito bem vindo.')
+nome = input('\033[32mQual é o seu \033[33mnome\033[33m\033[32m ?')
+print(f'\033[32mOlá\033[m \033[33m{nome}\033[m\033[32m, seja bem muito bem vindo.')
