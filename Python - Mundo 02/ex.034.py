cfb1bc80dbe81040d210739fe997ac99415596e8
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-12 17:25:59 -0300

    Pequena correção.

--- Python - Mundo 02/ex.034.py
+++ Python - Mundo 02/ex.034.py
@@ -3,7 +3,7 @@ salario = float(input('Digite o seu salário:'))
 ano = float(input('Digite em quantos anos você pretende pagar:'))
 meses = ano * 12
 pagamento = (casa / meses)
-if pagamento > salario + (pagamento / 30):
+if pagamento > salario * 0.3:
     print('Infelizmente você não pode obter o empréstimo')
 else:
     print(f'Empréstimo aprovado, Valor da prestação: R${pagamento:.2f}')
