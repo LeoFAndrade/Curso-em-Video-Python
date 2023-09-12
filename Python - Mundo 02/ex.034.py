aa6564ee9a72ce937fffee1dbb4e079d7b08d75c
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-12 17:22:57 -0300

    Programa simples de cálculo de empréstimo.

---
+++ Python - Mundo 02/ex.034.py
@@ -0,0 +1,9 @@
+casa = float(input('Qual é o valor da casa ?'))
+salario = float(input('Digite o seu salário:'))
+ano = float(input('Digite em quantos anos você pretende pagar:'))
+meses = ano * 12
+pagamento = (casa / meses)
+if pagamento > salario + (pagamento / 30):
+    print('Infelizmente você não pode obter o empréstimo')
+else:
+    print(f'Empréstimo aprovado, Valor da prestação: R${pagamento:.2f}')
