a22278d6d7b1903276bcf38a6418d687381ef0e9
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-13 18:39:12 -0300

    Comparação entre dois números

---
+++ Python - Mundo 02/ex.036.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite um número:'))
+num2 = int(input('Digite o segundo número:'))
+if num1 > num2:
+    print('O maior número é o primeiro!!!')
+elif num2 > num1:
+    print('O maior número é o segundo!!!')
+elif num1 == num2:
+    print('Ambos os valores são iguais!!!')
