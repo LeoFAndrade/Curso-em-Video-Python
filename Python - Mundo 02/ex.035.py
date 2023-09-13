0c28155cd284b360a406bce202bd9ab3c11abb38
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-13 18:42:07 -0300

    Organização do código aprimorada.

--- Python - Mundo 02/ex.035.py
+++ Python - Mundo 02/ex.035.py
@@ -1,14 +1,18 @@
 numero = int(input('Digite um número inteiro:'))
-print('Escolha uma das bases de conversão:')
-escolha = input('[ 1 ] Converter para BINÁRIO | [ 2 ] Converter para OCTAL | [ 3 ] Converter para HEXADECIMAL')
-if escolha == '1':
-    print(f'O seu número ficará assim: {bin(numero)}')
+print('''Escolha uma das bases de conversão:
+[ 1 ] Converter para BINÁRIO
+[ 2 ] Converter para OCTAL
+[ 3 ] Converter para HEXADECIMAL''')
+escolha = int(input('Digite a sua escolha:'))
+# O programa vai converter o número segundo a escolha.
+if escolha == 1:
+    print(f"O seu número ficará assim: {bin(numero).replace('0b','')}")
     end = ''
-elif escolha == '2':
-    print(f'O seu número ficará assim: {oct(numero)}')
+elif escolha == 2:
+    print(f'O seu número ficará assim: {oct(numero).replace("0o", "")}')
     end = ''
-elif escolha == '3':
-    print(f'O seu número ficará assim: {hex(numero)}')
+elif escolha == 3:
+    print(f'O seu número ficará assim: {hex(numero).replace("0x", "")}')
     end = ''
 else:
-    input('[ 1 ] Converter para BINÁRIO | [ 2 ] Converter para OCTAL | [ 3 ] Converter para HEXADECIMAL')
+    print('Opção inválida')
