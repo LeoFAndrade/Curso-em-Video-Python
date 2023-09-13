0c3cff3e619ee9d9b3221cabb6815013d5c351f9
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-13 18:43:43 -0300

    Pequenas correções.

--- Python - Mundo 02/ex.034.py
+++ Python - Mundo 02/ex.034.py
@@ -1,9 +1,10 @@
-casa = float(input('Qual é o valor da casa ?'))
-salario = float(input('Digite o seu salário:'))
-ano = float(input('Digite em quantos anos você pretende pagar:'))
-meses = ano * 12
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Digite o salário do cliente: R$'))
+anos = int(input('Digite em quantos anos você pretende pagar:'))
+meses = anos * 12
 pagamento = (casa / meses)
+# A multiplicação por decimal facilita bastante a conta.
 if pagamento > salario * 0.3:
-    print('Infelizmente você não pode obter o empréstimo')
+    print(f'Infelizmente você não pode obter o empréstimo, o valor do empréstimo: R${pagamento:.2f}')
 else:
     print(f'Empréstimo aprovado, Valor da prestação: R${pagamento:.2f}')
