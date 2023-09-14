a4a83143a3c364ef693602a08b4355ca0c38ab80
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-14 14:35:05 -0300

    Adicionados cores, pequenas revisões.

--- Python - Mundo 02/ex.037.py
+++ Python - Mundo 02/ex.037.py
@@ -1,11 +1,19 @@
 from datetime import date
-datahj = date.today().year
+
+atual = date.today().year
 data = int(input('Digite a data de nascimento:'))
-idade = datahj - data
-print(f'Quem nasceu em {data}, tem {idade} anos')
+idade = atual - data
+saldo = 18 - idade
+print(f'Quem nasceu em \033[34m{data}\033[m, tem \033[34m{idade} anos')
+
 if idade == 18:
-    print('É hora de se alistar')
+    print('\033[33mÉ hora de se alistar.')
 elif idade < 18:
-    print('Ainda vai se alistar')
+    ano = atual + saldo
+    print(f'\033[32mAinda falta {saldo} ano para o seu alistamento.')
+    print(f'\033[32mSeu alistamento será em {ano}')
 elif idade > 18:
-    print('Já passou da hora se alistar')
+    print('\033[31mJá passou da hora se alistar.')
+    saldo = idade - 18
+    anos = atual - saldo
+    print(f'Seu alistamento deveria ter sido no ano de {anos}')
