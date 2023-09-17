7f97554e1dc698e48d3ebd08e0ba1fa53c0ac771
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-17 20:18:06 -0300

    Aprovando Empréstimo
    Adicionado algumas notas

--- Python - Mundo 02/ex.041.py
+++ Python - Mundo 02/ex.041.py
@@ -3,11 +3,13 @@
 # conforme a tabela abaixo:
 
 print('\033[32m-=-' * 20), print('\033[33mCalculador de IMC\033[m'), print('\033[32m-=-' * 20, '\033[m')
-
+# Calculando o IMC usando o método usado para o cálculo
 altura = float(input('Digite a altura: (m)'))
 peso = float(input('Digite o peso: (Kg)'))
 imc = peso / (altura ** 2)
+# Formatando a str para melhor visualização
 print(f'{imc:.1f}')
+# O programa vai classificar conforme o peso calculado anteriormente
 if imc <= 18.5:
     print(f'\033[31mAbaixo do peso: {imc:.1f}')
 elif 18.5 <= imc <= 25:
