5e8ddf16337c5dcc71e944a97656e51befa6431c
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-17 20:14:33 -0300

    Aprovando Empréstimo
    Corrigido pequenos erros de cálculo, adicionado cor para facilitação de visão, e reorganizado o código para melhor visualização.

--- Python - Mundo 02/ex.034.py
+++ Python - Mundo 02/ex.034.py
@@ -1,10 +1,18 @@
+# Exercício Python 036: Escreva um programa para aprovar o empréstimo bancário
+# para a compra de uma casa. Pergunte o valor da casa, o salário do comprador
+# e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do
+# salário ou então o empréstimo será negado.
+
+x = '\033[33mAprovando Empréstimo\033[32m'
+print(f'\033[32m{x:=^40}\033[m')
+
 casa = float(input('Valor da casa: R$'))
 salario = float(input('Digite o salário do cliente: R$'))
 anos = int(input('Digite em quantos anos você pretende pagar:'))
 meses = anos * 12
 pagamento = (casa / meses)
-# A multiplicação por decimal facilita bastante a conta.
+# A multiplicação por decimal facilita a conta.
 if pagamento > salario * 0.3:
-    print(f'Infelizmente você não pode obter o empréstimo, o valor do empréstimo: R${pagamento:.2f}')
+    print(f'Infelizmente você não pode obter o empréstimo, o valor do empréstimo:\033[31mR${pagamento:.2f}')
 else:
-    print(f'Empréstimo aprovado, Valor da prestação: R${pagamento:.2f}')
+    print(f'Empréstimo aprovado, Valor da prestação:\033[32mR${pagamento:.2f}')
