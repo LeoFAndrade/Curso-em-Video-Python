11f73fbcdc35fc29a54abc814fbf7e26dc272326
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-17 20:32:32 -0300

    Calculador de média
    Adicionado notas do exercício

--- Python - Mundo 02/ex.039.py
+++ Python - Mundo 02/ex.039.py
@@ -1,8 +1,14 @@
+# Exercício Python 041: A Confederação Nacional de Natação precisa de um programa que
+# leia o ano de nascimento de um atleta e mostre sua categoria, conforme a idade:
+
+# Usando a biblioteca date para fins de informação sobre o tempo
 from datetime import date
 
+# Calculando a partir do dia do nascimento até o dia atual
 atual = date.today().year
 nasc = int(input('Digite a data de nascimento:'))
 idade = atual - nasc
+# Classificando o atleta conforme a idade atual
 if idade <= 9:
     print('A categoria é: \033[34mMIRIM')
 elif idade <= 14:
