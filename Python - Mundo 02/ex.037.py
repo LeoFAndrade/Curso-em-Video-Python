4a2f1c6e9c5222df16d393adc29a9fbb1dbe9ae4
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-17 20:28:25 -0300

    Alistamento militar
    Adicionado notas do exercício e de análise

--- Python - Mundo 02/ex.037.py
+++ Python - Mundo 02/ex.037.py
@@ -1,9 +1,19 @@
+# Exercício Python 039: Faça um programa que leia o ano de nascimento de um
+# jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao
+# serviço militar, se é a hora exata de se alistar ou se já passou do tempo
+# do alistamento. Seu programa também deverá mostrar o tempo que
+# falta ou que passou do prazo.
+
+# Usando da biblioteca date para fins de informação do tempo
 from datetime import date
 
+
+# Usando a data atual para calcular o tempo desde o nascimento
 atual = date.today().year
 data = int(input('Digite a data de nascimento:'))
 idade = atual - data
 saldo = 18 - idade
+# Estabelecendo condições para o alistamento conforme a data de nascimentos apresentada
 print(f'Quem nasceu em \033[34m{data}\033[m, tem \033[34m{idade} anos')
 if idade == 18:
     print('\033[33mÉ hora de se alistar.')
