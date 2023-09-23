71e1dbcd4f61088fc291d34ce932491d12702808
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-23 15:43:16 -0300

    Palíndromo
    Adicionado notas

---
+++ Python - Mundo 02/ex.051.py
@@ -0,0 +1,8 @@
+# Exercício Python 053: Crie um programa que leia uma frase qualquer e
+# diga se ela é um palíndromo, desconsiderando os espaços.
+
+pali = str(input('Digite algo: ')).strip().upper().replace(' ', '')
+if pali == pali[::-1]:  # Aqui reverterá a frase para analisar se é igual ou não
+    print(f'A palavra {pali} é palíndromo')
+else:
+    print(f'A palavra {pali} não é palíndromo')
