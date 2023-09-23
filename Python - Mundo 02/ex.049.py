048e40d38cda25a3119d9ac8efcc15cd635f5df8
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-22 21:41:51 -0300

    Progressão Aritmética
    Melhorado o código

--- Python - Mundo 02/ex.049.py
+++ Python - Mundo 02/ex.049.py
@@ -3,8 +3,7 @@
 
 num = int(input('Digite o número: '))  # Pegando os valores respectivamente do usuário
 razao = int(input('Digite a razão: '))
-n = int(input('Digite quantos elementos exibir: '))
-ultimo = num + (n + 1) * razao   # Somando os termos
-for x in range(num, ultimo, razao):  # Organizando a ordem aritmética
+ultimo = num + (10 - 1) * razao   # Somando os termos
+for x in range(num, ultimo + razao, razao):  # Organizando a ordem aritmética
     print(f'{x} ➙', end=' ')
 print('Fim')
