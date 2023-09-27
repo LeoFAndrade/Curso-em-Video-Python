bff1f006a0ed4ed6826610d1d046c9b7ef616d2e
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-09-27 14:27:08 -0300

    Maior e menor peso
    Adicionado notas

--- Python - Mundo 02/ex.053.py
+++ Python - Mundo 02/ex.053.py
@@ -6,12 +6,12 @@ maior = 0
 menor = 0
 for x in range(1, 6):
     peso = float(input(f'Digite o {cor} da {x}ª pessoa: '))
-    if x == 1:
+    if x == 1:  # Aqui começara pela primeira pessoa, considerando ela simultaneamente a de maior e menor peso
         maior = peso
         menor = peso
-    elif peso > maior:
+    elif peso > maior:  # Se outro valor for maior que o primeiro valor, então passa a ser considera o de maior peso
         maior = peso
-    elif peso < menor:
+    elif peso < menor:  # A mesma coisa acima, porém neste caso com o menor peso
         menor = peso
 print(f'O maior peso lido é {maior}ª')
 print(f'O menor peso lido é {menor}ª')
