ad0d257a6a868e49bf87e30ae54783d1b77d1574
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-10-01 18:14:16 -0300

    Calculo de Fatorial
    Feito espaçamento entre os códigos para melhor visualização, adicionado notas

--- Python - Mundo 02/ex.058.py
+++ Python - Mundo 02/ex.058.py
@@ -4,13 +4,13 @@ x = 'Fatorial'
 print(f'{x:=^40}')
 
 num = int(input('Digite um número: '))
-count = 1  # Serve como um operando
-resultado = 1  # A mesma coisa acima, com a diferença de que serve como resultado simultaneamente
+count = 1  # Serve como um contador que aumentará conforme 
+resultado = 1
 
 while count <= num:  # Se o contador não for menor ou igual que o número, repita
 
     resultado *= count  # O resultado vai se multiplicar com o count e após isso, se tornará o novo valor calculado
-    # Exemplo: resultado * count = X, X = resultado. O programa vai se repetir até a condição em while ser satisfeita
+    # Exemplo: resultado * count = X, X = resultado. O programa vai se repetir até while ser satisfeito
 
     count += 1  # No final do laço o contador vai adicionar mais 1 na sua contagem
     print(f'O resultado da fatoração é: {resultado} ')
