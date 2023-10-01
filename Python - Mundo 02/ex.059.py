c579f6b92668ec4aea67118dcedc02fe9f948ec2
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-10-01 19:05:26 -0300

    Progressão Aritmética v2.0
    Corrigido nota do exercício

--- Python - Mundo 02/ex.059.py
+++ Python - Mundo 02/ex.059.py
@@ -1,4 +1,4 @@
-# Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
+# Exercício 061: Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
 # Usando a estrutura while
 
 num = int(input('Digite um número: '))
