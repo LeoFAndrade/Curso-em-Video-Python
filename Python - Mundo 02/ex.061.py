1ac192b74055cebcad46d10c51491053d206f521
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-10-03 14:38:01 -0300

    Sequência de Fibonacci
    Atualizado com correções

--- Python - Mundo 02/ex.061.py
+++ Python - Mundo 02/ex.061.py
@@ -5,13 +5,12 @@ s = '\033[31mSequência de Fibonacci\033[32m'
 print(f'\033[32m{s:=^40}\033[m')
 
 num = int(input('Digite quantos termos deseja exibir: '))  # Entrada do usuário
-n = 1  # Primeiro termo
+n = 0  # Primeiro termo
 n2 = 1  # Segundo termo
-cont = 0  # Variável de controle
+cont = 2  # Variável de controle
 
-print('0 ➙ 1 ➙ ', end='')  # Início da sequência
-while cont < num:  # Enquanto a contagem for menor que "num" o programa vai repetir
-
+print(f'{n} ➙', end=' ') # Início da sequência
+while cont <= num:  # Enquanto a contagem for menor que "num" o programa vai repetir
     termo = n + n2  # Somando os termos, "primeiro" + "penúltimo"
     n2 = n  # O "penúltimo" termo se torna o "primeiro"
     n = termo  # E o "penúltimo" se torna o último, para o "último" se tornar o "primeiro" termo, resultado da soma
