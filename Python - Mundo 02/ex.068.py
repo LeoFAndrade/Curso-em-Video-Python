5ee553d9d1d7c75ba6bc0e18682e746e7dcdf6ae
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-10-15 15:57:52 -0300

    Estatísticas em produtos

--- Python - Mundo 02/ex.068.py
+++ Python - Mundo 02/ex.068.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia o nome e o preço de vários produtos.
+# Exercício 070: Crie um programa que leia o nome e o preço de vários produtos.
 # O programa deverá perguntar se o usuário vai continuar.
 # No final, mostre:
 # A) Qual é o total gasto na compra.
