c51dfe3ba31df0c682059d37d9a3deb5b8336ec1
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-10-15 15:59:07 -0300

    Tuplas com times de Futebol

--- Python - Mundo 03/ex.071.py
+++ Python - Mundo 03/ex.071.py
@@ -1,9 +1,6 @@
-# Crie uma tupla preenchida com oss 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de
-# colocação. Depois mostre:
-# A) Apenas os 5 primeiros colocados.
-# B) Os últimos 4 colocados da tabela.
-# C) Uma lista com os times em ordem alfabética.
-# D) Em que posição na tabela está o time da Chapecoense.
+# Exercício 073: Crie uma tupla preenchida com oss 20 primeiros colocados da Tabela do Campeonato Brasileiro de
+# Futebol, na ordem de colocação. Depois mostre: A) Apenas os 5 primeiros colocados. B) Os últimos 4 colocados da
+# tabela. C) Uma lista com os times em ordem alfabética. D) Em que posição na tabela está o time da Chapecoense.
 
 tabela = ('Botafogo', 'Bragantino', 'Grêmio', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Fluminense', 'Athletico-PR',
           'Atlético-MG', 'São Paulo', 'Cuiabá', 'Internacional', 'Cruzeiro', 'Corinthians', 'Santos', 'Bahia',
