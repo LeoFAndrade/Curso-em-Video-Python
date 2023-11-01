4cbab0a18aafcac9aba9467cb7fa9603c8613349
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-11-01 16:25:16 -0300

    Palpites para a MEGA SENA

--- Python - Mundo 03/ex.086.py
+++ Python - Mundo 03/ex.086.py
@@ -13,7 +13,7 @@ while True:  # O programa vai rodar até terminar todas as condições
     jogada = int(input('Quantos jogos você deseja sortear ? '))  # Input do usuário
 
     for jog in range(0, jogada):  # O programa vai repetir quantas vezes o usuário desejou
-        dados = (random.sample(range(0, 61), 6))  # Sample vai pegar algum número randomicamente de
+        dados = (random.sample(range(0, 61), 6))  # Sample vai pegar 6 números randomicamente de
         # range entre 0 e 61 e depois adicionar a lista de dados
         sleep(0.5)  # Pequeno intervalo de tempo
         print(sorted(dados))  # Retorna a lista de forma separada uma da outra
