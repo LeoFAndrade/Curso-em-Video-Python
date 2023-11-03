0e009c55928bf9c6bcb4b47a272fb34237ea66f2
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-11-03 16:24:07 -0300

    Boletim com listas compostas
    Adicionado notas

--- Python - Mundo 03/ex.087.py
+++ Python - Mundo 03/ex.087.py
@@ -2,27 +2,27 @@
 # No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno
 # individualmente.
 
-alunos = []
-flag = 999
-num = 0
+alunos = []  # Lista
+num = 0  # Variável de controle
+
 while True:
     nome = str(input('\033[1mDigite o nome do aluno: '))
-    nota1 = float(input('Digite a primeira nota do aluno:'))
+    nota1 = float(input('Digite a primeira nota do aluno:'))  # Input do usuário
     nota2 = float(input('Digite a segunda nota do aluno:'))
 
-    media = (nota1 + nota2)/2
-    alunos.append([nome, nota1, nota2, media])
+    media = (nota1 + nota2)/2  # Calculando a média
+    alunos.append([nome, nota1, nota2, media])  # Inserindo as informações na ficha do aluno
     
     resp = str(input('Deseja continuar ? [S/N]: ')).upper()
-    if resp in "N":
+    if resp in "N":  # Pergunta ao usuário se deseja inserir mais fichas
         print('---' * 15)
         print(f'{"No.":<4}{"NOME":<8}{"MÉDIA":>8}')
 
-        for index, itens in enumerate(alunos):
+        for index, itens in enumerate(alunos):  # Vai pegar os itens de cada índice de acordo com os parâmetros abaixo
             print(f'{index:<4}{itens[0]:<8}{itens[3]:>8.1f}')
             print('---' * 15)
 
-        while True:
+        while True:  # Perguntará se o usuário deseja continuar a ver as notas de um aluno
             num = int(input('Deseja ver a nota de qual aluno ? [999 para parar]: '))
             if num == 999:
                 print('FINALIZANDO')
