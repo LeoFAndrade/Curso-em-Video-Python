58045bab1258932cca2d87095805057a8e2de609
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-11-16 10:26:50 -0300

    Função de Contador

---
+++ Python - Mundo 03/ex.096.py
@@ -0,0 +1,33 @@
+# Faça um programa que tenha uma função chamada contador(), que receba três parâmetros:início, fim e passo e realize a
+# contagem.
+# Seu programa tem que realizar três contagens através da função criada:
+# a) De 1 até 10, de 1 em 1
+# b) De 10 até 0, de 2 em 2
+# c) Uma contagem personalizada
+
+from time import sleep
+
+
+def contador(inicio, fim, passo):  # O contador vai ter três paramêtros, um serve de início da contagem, o outro fim, e
+    # o passo que vai ditar de quanto em quanto deve se contar
+    if inicio <= fim:  # Se i início for menor que o fim, a contagem segue um ritmo regular
+        cont = inicio  # O inicio vai ser a contagem
+        while cont <= fim:  # Até a contagem não for igual a fim, o programa vai continuar contando
+            print(f'{cont} ', end='')
+            cont += passo  # O contador vai tendo o valor do passo adicionado, até chegar ao 'fim'
+            sleep(0.3)
+        print('FIM')
+
+    else:  # Se a contagem for ao contrário
+        cont = inicio  # Inicia normalmente como no programa acima
+        while cont >= fim:  # Enquanto a contagem não for igual a fim, vai continuar até atingir a condição
+            print(f'{cont} ', end='')
+            cont -= passo  # Aqui o contador vai tendo o valor subtraído, até atingir a condição
+            sleep(0.3)
+        print('FIM')
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+contador(inicio=int(input('\nDigite o inicio: ')), fim=int(input('Digite o fim: ')),
+         passo=int(input('Digite o passo: ')))
