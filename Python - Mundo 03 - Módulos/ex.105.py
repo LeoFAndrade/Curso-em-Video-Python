ebac3c133ee708867703f25002ff2b61c8137dc3
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-11-28 16:28:44 -0300

    Exercitando módulos em Python

--- Python - Mundo 03 - Módulos/ex.105.py
+++ Python - Mundo 03 - Módulos/ex.105.py
@@ -1,8 +1,13 @@
-from uteis import numeros
+# Exercício 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e
+# metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
 
+from uteis.numeros import moeda
 
-num = int(input('Digite um número: '))
-fat = numeros.fatorial(num)
-print(f'O fatorial de {num} é {fat}')
-print(f'O dobro de {num} é {numeros.dobro(num)}')
-print(f'O Triplo de {num} é {numeros.triplo(num)}')
+print('~~~' * 20)
+print('Calculadora de preço')
+print('~~~' * 20)
+produto = int(input('Digite o total da compra de produtos R$: '))
+print(f'O dobro de {produto} é R$ {moeda.dobro(produto)} ')
+print(f'Aumentando 10% temos R$ {moeda.aumentar(produto, 10)}')
+print(f'Reduzindo 20%, temos R$ {moeda.diminuir(produto, 20)}')
+print(f'A metade de {produto} é R$ {moeda.Metade(produto)}')
---
+++ Python - Mundo 03 - Módulos/uteis/numeros/moeda.py
@@ -0,0 +1,16 @@
+def Metade(numero):
+    n = numero // 2
+    return n
+
+def aumentar(num, pc):
+    num += num * pc / 100
+    return num
+
+
+def diminuir(num, pc):
+    num -= num * pc / 100
+    return num
+
+
+def dobro(num):
+    return num * 2
