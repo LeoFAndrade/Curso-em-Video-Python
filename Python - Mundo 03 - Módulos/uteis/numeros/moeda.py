9b26b54f0cf5c04adb68afc2f458d1befb8792d5
LeonardoAndrade <138392065+LeoFAndrade@users.noreply.github.com>
2023-11-30 18:42:59 -0300

    Reduzindo ainda mais seu programa (não terminado)

--- Python - Mundo 03 - Módulos/uteis/numeros/moeda.py
+++ Python - Mundo 03 - Módulos/uteis/numeros/moeda.py
@@ -1,35 +1,63 @@
+# noinspection NonAsciiCharacters,PyArgumentList
+def resumo(preço, mais, menos):
+    # noinspection PyArgumentList
+    mais = int
+    menos = int
+    print('~~~' * 20)
+    print('Resumo do Valor')
+    print('~~~' * 20)
+    print(f'Preço analisado    {preço}')
+    print(f'Dobro do preço:    {dobro(preço), True}')
+    print(f'Metade do preço:    {metade(preço, True)}')
+    print(f'{mais}% de aumento: {aumentar(preço, mais, True)}')
+    print(f'{menos}% de redução {diminuir(preço, menos, True)}')
 
-def aumentar(num, pc, sit=False):
-    num += num * pc / 100
+
+na = float(input('Digite um número'))
+print(resumo(na, 50, 10))
+
+
+# noinspection NonAsciiCharacters
+def aumentar(preço=0, taxa=0, sit=False):
+    """"→ Adiciona o valor da porcentagem sobre um número"""
+    resp = preço + (preço * taxa / 100)
     if sit:
-        return moeda(num)
+        return moeda(resp)
     else:
-        return num
+        return resp
 
 
-def diminuir(num, pc, sit=False):
-    num -= num * pc / 100
+# noinspection NonAsciiCharacters
+def diminuir(preço=0, taxa=0, sit=False):
+    """"→ Subtrai o valor da porcentagem sobre um número"""
+    resp = preço + (preço * taxa / 100)
     if sit:
-        return moeda(num)
+        return moeda(resp)
     else:
-        return num
+        return resp
 
 
-def metade(numero, sit=False):
-    num = numero // 2
+# noinspection NonAsciiCharacters
+def metade(preço, sit=False):
+    """"→ Calcula a metade de um número."""
+    resp = preço // 2
     if sit:
-        return moeda(num)
+        return moeda(resp)
     else:
-        return num
+        return resp
 
 
-def dobro(num, sit=False):
+# noinspection NonAsciiCharacters
+def dobro(preço, sit=False):
+    """"→ Calcula o dobro de um número."""
+    resp = preço * 2
     if sit:
-        return moeda(num)
+        return moeda(resp)
     else:
-        return num
+        return resp
 
 
 def moeda(num):
+    """"→ Retorna uma str formatada em forma monetária."""
     n = f'R${num:,.2f}'
     return n
